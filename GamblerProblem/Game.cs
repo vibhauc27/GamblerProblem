d49c1d5e9f27d89dfceff4bcf14301da7b1f326e
vibha <vibhauc@gmail.com>
2022-03-11 10:01:02 +0530

    [Vibha]Added:The gambler game of UC2 to simulate

---
+++ GamblerProblem/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamblerProblem
+{
+    public class Game
+    {
+        public void StartGame()
+        {
+            int stakemoney = 100;
+            int betmoney = 1;
+            Console.WriteLine($"Money is : {stakemoney}");
+            Console.WriteLine($"Money is : {betmoney}");
+        }
+    }
+}
--- GamblerProblem/Program.cs
+++ GamblerProblem/Program.cs
@@ -7,10 +7,10 @@ namespace GamblerProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Gambler Problem");
-            int stackmoney = 100;
-            int betmoney = 1;
-            Console.WriteLine($"Money is : {stackmoney}");
-            Console.WriteLine($"Money is : {betmoney}");
+           // int stackmoney = 100;
+            //int betmoney = 1;
+           // Console.WriteLine($"Money is : {stakemoney}");
+            //Console.WriteLine($"Money is : {betmoney}");
         }
     }
 }
