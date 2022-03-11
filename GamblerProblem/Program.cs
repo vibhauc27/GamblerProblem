4eafb248d3b290872f6f43842fc066af9b24fdad
vibha <vibhauc@gmail.com>
2022-03-11 08:36:01 +0530

    Gambler Problem

---
+++ GamblerProblem/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GamblerProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Gambler Problem");
+            int stackmoney = 100;
+            int betmoney = 1;
+            Console.WriteLine($"the money is : {stackmoney}");
+            Console.WriteLine($"the money is : {betmoney}");
+        }
+    }
+}
